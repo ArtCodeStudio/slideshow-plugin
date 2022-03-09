4f663634a464fe46f131e6e11154d20990bb5543
Pascal Garber <pascal@artandcode.studio>
2022-03-09 15:41:19 -0500

    Latest remote changes

---
+++ updates/builder_table_update_jumplink_slideshow_slides_4.php
@@ -0,0 +1,23 @@
+<?php namespace JumpLink\Slideshow\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateJumplinkSlideshowSlides4 extends Migration
+{
+    public function up()
+    {
+        Schema::table('jumplink_slideshow_slides', function($table)
+        {
+            $table->string('section')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('jumplink_slideshow_slides', function($table)
+        {
+            $table->dropColumn('section');
+        });
+    }
+}
